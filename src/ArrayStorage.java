a6b83b96102093e7a0d5378edcab12e75de5038e
gandzytm <gandzy1984@gmail.com>
2019-08-12 19:06:44 +0500

    Merge branch 'master' of C:\Users\Gandzy\Documents\GitHub\basejava with conflicts.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -8,12 +8,13 @@ public class ArrayStorage {
     int size = 0;
 
     void save(Resume r) {
-        int index = findElement(r.getUuid());
-        if (index == -1 || size > storage.length) {
-            storage[size] = r;
-            size++;
-        } else {
-            System.out.println(r.getUuid() + " is in storage");
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(r.getUuid())) {
+                storage[size] = r;
+                size++;
+            } else {
+                System.out.println(r.getUuid() + " is in storage");
+            }
         }
     }
 
@@ -21,10 +22,12 @@ public class ArrayStorage {
         int index = findElement(uuid);
         if (index != -1) {
             return storage[index];
-        } else {
-            System.out.println(uuid + " didn't in Resume storage");
         }
         return null;
+    }
+
+    int size() {
+        return size;
     }
 
     /**
@@ -36,26 +39,14 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        int index = findElement(uuid);
-        if (index != -1) {
-            for (int i = index; i < size - 1; i++) {
-                if (storage[i].getUuid().equals(uuid)) {
-                    int var3 = size - index - 1;
-                    System.arraycopy(storage, i + 1, storage, index, var3);
-                    size--;
-                } else {
-                    System.out.println(uuid + " didn't in Resume storage");
-                }
+        for (int i = findElement(uuid); i < size - 1; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                storage[i] = storage[i + 1];
+                size--;
+            } else {
+                System.out.println(uuid + " didn't in Resume storage");
             }
-        } else {
-            System.out.println(uuid + " didn't in Resume storage");
         }
-    }
-
-    void update(Resume resume) {
-        // delete
-        // insert or save
-
     }
 
     void clear() {
@@ -63,8 +54,14 @@ public class ArrayStorage {
         size = 0;
     }
 
-    int size() {
-        return size;
+    void update(Resume resume) {
+        // delete
+        delete(resume.getUuid());
+        // insert or save
+        for (int i = 0; i < size; i++) {
+
+        }
+
     }
 
     private int findElement(String uuid) {
--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -13,11 +13,14 @@ public class MainTestArrayStorage {
         r3.setUuid("uuid3");
         Resume r4 = new Resume();
         r4.setUuid("uuid4");
+        Resume r5 = new Resume();
+        r5.setUuid("uuid4");
 
         ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r2);
         ARRAY_STORAGE.save(r3);
         ARRAY_STORAGE.save(r4);
+        ARRAY_STORAGE.save(r5);
 
         System.out.println("Get r3: " + ARRAY_STORAGE.get(r3.getUuid()));
         System.out.println("Size: " + ARRAY_STORAGE.size());
