286fd54e6d4fe292d61c284f8eed7fb2606fdae1
GandzyTM <gandzy1984@gmail.com>
2019-08-12 15:14:10 +0500

    update delete()
    fix for delete method

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,47 +5,46 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    int incSize = 0;
+    int size = 0;
 
     void save(Resume r) {
-        storage[incSize] = r;
-        incSize++;
+        int index = findElement(r.getUuid());
+        if (index == -1 || size > storage.length) {
+            storage[size] = r;
+            size++;
+        } else {
+            System.out.println(r.getUuid() + " is in storage");
+        }
     }
 
     Resume get(String uuid) {
         int index = findElement(uuid);
         if (index != -1) {
             return storage[index];
+        } else {
+            System.out.println(uuid + " didn't in Resume storage");
         }
         return null;
-    }
-
-    int size() {
-        return incSize;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, incSize);
+        return Arrays.copyOf(storage, size);
 //        return new Resume[0];
     }
 
     void delete(String uuid) {
-        for (int k = findElement(uuid); k < incSize - 1; k++) {
-            if (storage[k].getUuid().equals(uuid)) {
-                storage[k] = storage[k + 1];
-                incSize--;
+        for (int i = findElement(uuid); i < size - 1; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                int var3 = size - findElement(uuid) - 1;
+                System.arraycopy(storage, i + 1, storage, findElement(uuid), var3);
+                size--;
             } else {
                 System.out.println(uuid + " didn't in Resume storage");
             }
         }
-    }
-
-    void clear() {
-        Arrays.fill(storage, 0, incSize, null);
-        incSize = 0;
     }
 
     void update(Resume resume) {
@@ -54,8 +53,17 @@ public class ArrayStorage {
 
     }
 
+    void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    int size() {
+        return size;
+    }
+
     private int findElement(String uuid) {
-        for (int i = 0; i < incSize; i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
                 return i;
             }
