712a328f32163dc2dbee6bfee6f3098f16a0d72c
GandzyTM <gandzy1984@gmail.com>
2019-08-20 11:23:41 +0500

    HW3 AbstractArrayStorage template methods

--- src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
@@ -2,6 +2,8 @@ package ru.javawebinar.basejava.storage;
 
 import ru.javawebinar.basejava.model.Resume;
 
+import java.util.Arrays;
+
 public abstract class AbstractArrayStorage implements Storage{
     protected static final int STORAGE_LIMIT = 10_000;
     protected Resume[] storage = new Resume[STORAGE_LIMIT];
@@ -9,6 +11,46 @@ public abstract class AbstractArrayStorage implements Storage{
 
     public int size() {
         return size;
+    }
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, size);
+    }
+
+    public void save(Resume resume) {
+        if (size >= STORAGE_LIMIT) {
+            System.out.println("Can't save " + resume.getUuid() + " because resume storage is full");
+        } else if (findIndex(resume.getUuid()) == -1) {
+            storage[size] = resume;
+            size++;
+        } else {
+            System.out.println("Resume " + resume.getUuid() + " already exists in storage");
+        }
+    }
+
+    public void delete(String uuid) {
+        int index = findIndex(uuid);
+        if (index != -1) {
+            size--;
+            storage[index] = storage[size];
+            storage[size] = null;
+        } else {
+            System.out.println("Resume " + uuid + " not exist in storage");
+        }
+    }
+
+    public void update(Resume resume) {
+        int index = findIndex(resume.getUuid());
+        if (index != -1) {
+            storage[index] = resume;
+        } else {
+            System.out.println("Resume " + resume.getUuid() + " not exist in storage");
+        }
     }
 
     public Resume get(String uuid) {
