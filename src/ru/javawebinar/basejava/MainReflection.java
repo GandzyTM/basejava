4d1801549ba327fb860648890a85fa851fa6d07d
gandzytm <gandzy1984@gmail.com>
2019-08-25 11:34:44 +0500

    HW4 invoke resume.toString

--- src/ru/javawebinar/basejava/MainReflection.java
+++ src/ru/javawebinar/basejava/MainReflection.java
@@ -3,16 +3,22 @@ package ru.javawebinar.basejava;
 import ru.javawebinar.basejava.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume resume = new Resume();
         Field field = resume.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
         field.get(resume);
         field.set(resume, "new_uuid");
-        // TODO invoke resume.toString via reflection
         System.out.println(resume);
+        // TODO invoke resume.toString via reflection
+        Method method = resume.getClass().getMethod("toString", null);
+        method.invoke(resume, null);
+        System.out.println(resume.toString());
+
     }
 }
