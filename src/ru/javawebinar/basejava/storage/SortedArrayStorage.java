653536e6468a52b79ca6513d5ac004a06143f129
GandzyTM <gandzy1984@gmail.com>
2019-08-30 14:10:37 +0500

    Update SortedArrayStorage.java

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -5,18 +5,18 @@ import ru.javawebinar.basejava.model.Resume;
 import java.util.Arrays;
 import java.util.Comparator;
 
-public class SortedArrayStorage extends AbstractArrayStorage{
+public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     protected void saveElement(Resume resume, int index) {
-        int insertIdx = - index - 1;
+        int insertIdx = -index - 1;
         System.arraycopy(storage, insertIdx, storage, insertIdx + 1, size - insertIdx);
         storage[insertIdx] = resume;
     }
 
     @Override
     protected void deleteElement(int index) {
-        int numMoved =  - index - 1;
+        int numMoved = -index - 1;
         if (numMoved > 0) {
             System.arraycopy(storage, index + 1, storage, index, numMoved);
         }
